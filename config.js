f1472a1bbadea0af77265783d8e340121b32e6dc
garry2415oo <80236740+garry2415oo@users.noreply.github.com>
2022-08-21 10:10:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyDUXb_RyLFtiQlKcImL_LdQSt2u_wgNdsY",
+    authDomain: "rider-59d3c.firebaseapp.com",
+    projectId: "rider-59d3c",
+    storageBucket: "rider-59d3c.appspot.com",
+    messagingSenderId: "271622494262",
+    appId: "1:271622494262:web:958324f71401dcfd11e097"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
